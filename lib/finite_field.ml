93692bf289f3e89d2c18c89f6fad0a0785bb8710
Corneliu Hoffman <corneliuhoffman@gmail.com>
2022-06-30 18:38:32 +0100

    both ways

--- lib/finite_field.ml
+++ lib/finite_field.ml
@@ -352,6 +352,19 @@ module Make (Field : S) : sig
 
   val fft : t array -> t array
 
+  val rev_delta :
+    'a ->
+    int ->
+    int ->
+    t ->
+    t ->
+    t array ->
+    (int * t, t) Hashtbl.t ->
+    (int * int * t, t) Hashtbl.t ->
+    t
+
+  val coef : (int * t, t) Hashtbl.t
+
   val ffti : t array -> t array
 end = struct
   include Field
@@ -364,13 +377,13 @@ end = struct
 
   (** this precomputes a hash table that cntains at position (i c) the value $
       $W_i(c +l)/W_i(2^i)$*)
-  let precomp_coef = precompute_coef 20 l_for_fft
+  let coef = precompute_coef 16 l_for_fft
 
   let rec delta logh i m c l d coefs mem =
     (* let h = Uint128.shift_left Uint128.one logh in *)
     (* assert (m < 1 lsl (i + 1)) ;
        assert (div_by_power i c) ; *)
-    (* Format.printf "i = %i; m=%i; c = %s\n" i m (Uint128.to_string c) ; *)
+
     (* assert (Uint128.compare c h < 0) ; *)
     try Hashtbl.find mem (i, m, c)
     with _ ->
@@ -379,28 +392,78 @@ end = struct
         d.(m))
       else if div_by_power (i + 1) c then (
         let left = delta logh (i + 1) m c l d coefs mem in
-        let right = delta logh (i + 1) (m lsl i) c l d coefs mem in
+        let right = delta logh (i + 1) (m + (1 lsl i)) c l d coefs mem in
         let coeficient = Hashtbl.find coefs (i, c) in
         let result = add left @@ mult coeficient right in
+
         Hashtbl.add mem (i, m, c) result ;
         result)
       else
         let left = delta logh i m (add c (shift_left one i)) l d coefs mem in
         let right =
-          delta logh (i + 1) (m lsl i) (add c (shift_left one i)) l d coefs mem
+          delta
+            logh
+            (i + 1)
+            (m + (1 lsl i))
+            (add c (shift_left one i))
+            l
+            d
+            coefs
+            mem
         in
         let result = add left right in
         Hashtbl.add mem (i, m, c) result ;
         result
+
+  let rec rev_delta logh i m c l d coefs mem =
+    match (i, m) with
+    | 0, 0 -> d.(Uint128.to_int c)
+    | _ ->
+        if Hashtbl.mem mem (i, m, c) then Hashtbl.find mem (i, m, c)
+          (* else if c = l then rev_delta logh 0 m c l d coefs mem *)
+        else if m >= 1 lsl (i - 1) then (
+          let left =
+            rev_delta logh (i - 1) (m - (1 lsl (i - 1))) c l d coefs mem
+          in
+          let right =
+            rev_delta
+              logh
+              (i - 1)
+              (m - (1 lsl (i - 1)))
+              (add c (shift_left one (i - 1)))
+              l
+              d
+              coefs
+              mem
+          in
+          let result = add left right in
+          Hashtbl.add mem (i, m, c) result ;
+          result)
+        else
+          let left = rev_delta logh (i - 1) m c l d coefs mem in
+
+          let right = rev_delta logh i (m + (1 lsl (i - 1))) c l d coefs mem in
+          let coeficient =
+            if add c l < shift_left one (i - 1) then zero
+            else Hashtbl.find coefs (i - 1, c)
+          in
+          let result = add left @@ mult coeficient right in
+          Hashtbl.add mem (i, m, c) result ;
+          result
 
   let fft d =
     let logh = Z.log2up (Z.of_int (Array.length d)) in
     let mem = Hashtbl.create 500000 in
     Array.init (1 lsl logh) (fun x ->
         let c = of_int x in
-        delta logh 0 0 c l_for_fft d precomp_coef mem)
+        delta logh 0 0 c l_for_fft d coef mem)
 
-  let ffti _ = assert false
+  let ffti d =
+    let logh = 16 in
+    let mem = Hashtbl.create 500000 in
+
+    Array.init (1 lsl logh) (fun x ->
+        rev_delta logh logh x zero l_for_fft d coef mem)
 end
 
 module Fi128 = Make (struct
--- main.ml
+++ main.ml
@@ -10,14 +10,15 @@ let () =
          (100080340, 12340000)
      in *)
 let gen () = QCheck2.( Gen.generate1 @@ Gen.map (fun a -> Stdint.Uint128.of_int64 a) Gen.int64) in
-let d = Array.init (1 lsl 16) (fun _ -> gen ()) in 
+let d = Array.init (1 lsl 18) (fun _ -> gen ()) in 
 Format.printf "donesetup ";
-
+(* 
   let a = Stdint.Uint128.of_int 127537 in
   let c = Stdint.Uint128.((max_int / a) - a) in
   let _ = Benchmark.throughput1 10 (fun (a, b) -> mult_rij128 a b) (a, c) in
-  let _ = Benchmark.throughput1 10 (fun a -> inv_rij128 a) a in
-  let _ = Benchmark.throughput1 10 (fun _ ->  let _ = Fi128.fft d in ()) () in
-
+  let _ = Benchmark.throughput1 10 (fun a -> inv_rij128 a) a in *)
+  let _ = Benchmark.throughput1 10 (fun _ ->  let _ = Fi128.ffti d in ()) () in
+let _d1 = Fi128.ffti d in
+(* Format.printf "%b" (d = (Fi128.fft d1)); *)
   (* let _ = Benchmark.throughput1 10 (fun a -> inv 64 a) 100080340 in *)
   ()
