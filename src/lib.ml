2be71419d904e9f0d1574a1c22352b8ec1fd5ba3
corneliuhoffman <corneliuhoffman@gmail.com>
2022-07-28 14:25:36 +0100

    Update README.md

--- src/lib.ml
+++ src/lib.ml
@@ -10,3 +10,4 @@ external mulbyshift: int64 -> int64-> (int64 * int64) = "mulbyshift"
 external multiplication: int64 -> int64->int64->int64-> (int64 * int64) = "multiplication"
 external inverse: int64 -> int64-> (int64 * int64) = "inverse"
 external addition: int64 -> int64->int64->int64-> (int64 * int64) = "addition"
+
